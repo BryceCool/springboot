615ff248824e8f729c5a3356ac76d67c78fbd143
yangguanghui6 <yangguanghui6@hikvision.com.cn>
2021-02-01 14:05:29 +0800

    kaptcha 验证码加入

---
+++ src/main/java/com/springboot/frame/config/VerificationCodeConfig.java
@@ -0,0 +1,45 @@
+package com.springboot.frame.config;
+
+import com.google.code.kaptcha.impl.DefaultKaptcha;
+import com.google.code.kaptcha.util.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Properties;
+
+/**
+ * 利用google的kaptcha 工具进行验证码的生成
+ *
+ * @author ygh
+ * @date 2021-02-01 12:17:00
+ */
+@Configuration
+public class VerificationCodeConfig {
+
+    @Bean
+    public DefaultKaptcha getVerificationCode() {
+        DefaultKaptcha code = new DefaultKaptcha();
+        Properties properties = new Properties();
+        // 图片边框
+        properties.setProperty("kaptcha.border", "no");
+        // 边框颜色
+        properties.setProperty("kaptcha.border.color", "105,179,90");
+        // 字体颜色
+        properties.setProperty("kaptcha.textproducer.font.color", "black");
+        // 图片宽
+        properties.setProperty("kaptcha.image.width", "120");
+        // 图片高
+        properties.setProperty("kaptcha.image.height", "50");
+        // 字体大小
+        properties.setProperty("kaptcha.textproducer.font.size", "30");
+        // session key
+        properties.setProperty("kaptcha.session.key", "code");
+        // 验证码长度
+        properties.setProperty("kaptcha.textproducer.char.length", "5");
+        // 字体
+        properties.setProperty("kaptcha.textproducer.font.names", "宋体,楷体,微软雅黑");
+        Config config = new Config(properties);
+        code.setConfig(config);
+        return code;
+    }
+}
